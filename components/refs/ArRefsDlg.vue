2d56b4b03ad780d1b01d169e06d759fea955c9d3
alex <alex@localhost>
2024-01-08 16:41:42 +0300

    save/edit

--- components/refs/ArRefsDlg.vue
+++ components/refs/ArRefsDlg.vue
@@ -90,90 +90,52 @@ export default {
     },
     methods: {
         open(item){
-            console.log('opening', item);
+            //console.log('opening', item);
             this.item = {...item};
             this.show = true;
             refreshNuxtData('company-settings-item');
         },
-        save(){
-            
+        async save() {
+            const form = this.$refs.form;
+            var ok = await form.validate();
+            if ( !ok ) {
+                return false;
+            }
+            this.pending = true;
+            //console.log('saving', this.item);
+            try {
+                const v = this.item,
+                    cols = v.cols.filter( c => !/(\_at)+$/.test(c.key) ),
+                    body = {};
+                cols.map((c) => {
+                    if ( c.type == 'date' ) {
+                        body[c.key] = phpdate2m(v[c.key]).toDate();
+                    } else {
+                        body[c.key] = v[c.key];
+                    }
+                });
+                let res = await $jet.api({
+                    url:    (v.id > 0) ? `${v.attrs.url}/${ v.id }` : v.attrs.url,
+                    method: (v.id > 0) ? 'PUT' : 'POST',
+                    body
+                });
+                //console.log('save', res);
+                if ( res.success ){
+                    this.show = false;
+                    this.item.id = res.id;
+                    this.$emit("success");
+                }
+            } catch(e){
+                ok = false;
+                $jet.msg({
+                            text: `Ошибка сохранения изменений.<br /><small>Информация для техподдержки: ${ e.message } ${$jet.api.$errm}`, 
+                            color: "warning",
+                            timeout: 20000
+                        });
+            } finally {
+                this.pending = false;
+            }
         }
     }
 };
-    /*
-    const { pending, error } = useAsyncData('company-settings-item', async ()=>{
-        if ( !show.value ){
-            return false;
-        }
-        if ( item.value.id < 1 ){
-            item.value.start = new Date();
-            item.value.end   = null;
-            item.value.value = null;
-            return false;
-        }
-        
-        try {
-            let res = await $jet.api({
-                url: `${ item.value.attrs.url }/${ item.value.id }`
-            });
-            if ( res.success ){
-                item.value.start = phpdate2m(res.result.start_date).toDate();
-                item.value.end   = (res.result.end_date) ? phpdate2m(res.result.end_date).toDate() : null;
-                item.value.value = res.result[item.value.attrs.val];
-            }
-        } catch(e){
-            console.log('ERR (carriers)', e);
-            $jet.msg({
-                        text:`Ошибка получения данных.<br /><small>Информация для техподдержки: ${e.message} ${$jet.api.$errm}`,
-                        type: 'warning'
-                    });
-        }
-    });
-    
-    async function save(){
-        let ok = await form.value.validate();
-        if (!ok){
-            return false;
-        }
-        
-        pending.value = true;
-        console.log('saving', item);
-        try {
-            const body = {
-                start_date: $moment(item.value.start).format('YYYY-MM-DD')
-            };
-            if (item.value.end){
-                body.end_date = $moment(item.value.end).format('YYYY-MM-DD');
-            } else {
-                body.end_date = null;
-            }
-            if (item.value.id > 0){
-                body.id = item.value.id;
-            }
-            body[item.value.attrs.val] = item.value.value;
-            console.log('saving-2', body);
-            
-            
-            let res = await $jet.api({
-                url:    (item.value.id > 0) ? `${item.value.attrs.url}/${ item.value.id }` : item.value.attrs.url,
-                method: (item.value.id > 0) ? 'PUT' : 'POST',
-                body
-            });
-            console.log('save', res);
-            if ( res.success ){
-                show.value = false;
-                item.value.id = res.id;
-                $emit("success");
-            }
-        } catch(e){
-            ok = false;
-            $jet.msg({
-                        text: `Ошибка сохранения изменений.<br /><small>Информация для техподдержки: ${ e.message } ${$jet.api.$errm}`, 
-                        color: "warning",
-                        timeout: 20000
-                    });
-        } finally {
-            pending.value = false;
-        }
-    }; */  //save
 </script>
